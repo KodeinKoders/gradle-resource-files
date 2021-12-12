6699955e9c818abe68097baa8118c645141550e9
Salomon BRYS <salomon.brys@gmail.com>
2021-12-12 15:47:32 +0100

    Publishing action

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
     `maven-publish`
+    id("com.gradle.plugin-publish") version "0.18.0"
 }
 
 group = "net.kodein.gradle.resources"
@@ -11,9 +12,21 @@ repositories {
     mavenCentral()
 }
 
+System.getenv("GRADLE_PUBLISH_KEY")?.let { ext["gradle.publish.key"] = it }
+System.getenv("GRADLE_PUBLISH_SECRET")?.let { ext["gradle.publish.secret"] = it }
+
 gradlePlugin {
     plugins.create("resource-files") {
         id = "net.kodein.gradle.resources.resource-files"
         implementationClass = "net.kodein.gradle.resources.ResourceFilesPlugin"
+        displayName = "Resource files dependencies"
+        description = "Gradle Plugin to distribute resource files archives"
     }
+}
+
+pluginBundle {
+    website = "https://github.com/KodeinKoders/gradle-resource-files"
+    vcsUrl = "https://github.com/KodeinKoders/gradle-resource-files.git"
+    description = "Gradle Plugin to distribute resource files archives"
+    tags = listOf("resources", "dependencies")
 }
